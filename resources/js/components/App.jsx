5fef70428d3982cddfccbe1829c5ff482cd9b018
KenKoorn <35050@ma-web.nl>
2024-12-09 10:28:40 +0100

    lara

---
+++ resources/js/components/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div>
+            <h1>Welcome to My Laravel React App!</h1>
+            <p>This page is powered by React.</p>
+        </div>
+    );
+};
+
+export default App;
