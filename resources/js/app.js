c3215681bed6ec01d06aafd359e43e65f93f419d
BenteKavsek <35726@ma-web.nl>
2024-12-09 11:23:55 +0100

    login systeem

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client'; 
-import App from './components/App';
 import './bootstrap';
-import './components/Example';
 
-const root = ReactDOM.createRoot(document.getElementById('app'));
-root.render(<App />);
+import Alpine from 'alpinejs';
+
+window.Alpine = Alpine;
+
+Alpine.start();
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,18 +1,47 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Laravel with React</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Laravel</title>
     @vite(['resources/js/app.js'])
 </head>
 
+
 <body>
-    <h1>Test</h1>
-    sudo chmod -R 775 storage bootstrap/cache
+    <header>
+        @if (Route::has('login'))
+        <nav class="-mx-3 flex flex-1 justify-end">
+            @auth
 
-    <div id="app"></div>
+            @else
+            <a
+                href="{{ route('login') }}"
+                class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white">
+                Inloggen
+            </a>
+
+            @if (Route::has('register'))
+            <a
+                href="{{ route('register') }}"
+                class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white">
+                Registeren
+            </a>
+            @endif
+            @endauth
+        </nav>
+        @endif
+    </header>
+    <main>
+
+        <h1>Test</h1>
+        sudo chmod -R 775 storage bootstrap/cache
+
+        <div id="app"></div>
+
+    </main>
+    <footer>
+        FOOTER
+    </footer>
 </body>
-
-</html>
